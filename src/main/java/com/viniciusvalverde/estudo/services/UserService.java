5aa8c815e5aece30d6bba58b1325a2137ded5ded
VINICIUS <valverdevinicius@protonmail.com>
2023-06-26 16:08:21 -0300

    Service Layer

---
+++ src/main/java/com/viniciusvalverde/estudo/services/UserService.java
@@ -0,0 +1,26 @@
+package com.viniciusvalverde.estudo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.viniciusvalverde.estudo.entities.User;
+import com.viniciusvalverde.estudo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> op = repository.findById(id);
+		return op.get();
+	}
+}
