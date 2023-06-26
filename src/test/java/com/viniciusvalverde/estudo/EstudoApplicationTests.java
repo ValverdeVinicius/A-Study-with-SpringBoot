edc4d538099fede9c416d38b512d58d897478621
VINICIUS <valverdevinicius@protonmail.com>
2023-06-26 14:42:53 -0300

    Project created

---
+++ src/test/java/com/viniciusvalverde/estudo/EstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusvalverde.estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
